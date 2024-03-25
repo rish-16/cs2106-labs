f3b8f5b4116893c0a3886fff74a85eb6ebb54d80
Rishabh Anand <mail.rishabh.anand@gmail.com>
2024-03-26 01:54:50 +0800

    Updates

--- CS2106lab3/part2/barrier.c
+++ CS2106lab3/part2/barrier.c
@@ -66,8 +66,7 @@ void reach_barrier() {
     sem_wait(barrier_object->sem1);
     (*barrier_object->count)++;
     sem_post(barrier_object->sem1); // unlock the counter mutex –> other variables are free to access counter
-    if (*barrier_object->count == barrier_object)
-    {
+    if (*barrier_object->count == barrier_object->nproc) {
         sem_post(barrier_object->barrier); // last process at the barrier sends a signal
     } else {
         sem_wait(barrier_object->barrier); // not the last process -> block cur process and wait until last process reaches
