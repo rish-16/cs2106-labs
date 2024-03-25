f5fc46ef5f1017ed16842be225eb9fc1d22fef18
Rishabh Anand <mail.rishabh.anand@gmail.com>
2024-03-26 02:30:08 +0800

    Updates

--- CS2106lab3/part3/barrier.c
+++ CS2106lab3/part3/barrier.c
@@ -5,31 +5,33 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 
-int *count;
-int nproc;
 sem_t *sem1; // the mutex semaphore
 sem_t *barrier;
+int *count;
+int nproc;
 
 void init_barrier(int numproc) {
     nproc = numproc;
 
-    int shmid;
-    shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
-    count = (int *)shmat(shmid, NULL, 0);
-
-    if (shmid == -1) {
+    int shmid1, shmid2, shmid3;
+    shmid1 = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    shmid2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
+    shmid3 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
+    if (shmid1 == -1 || shmid2 == -1 || shmid3 == -1) {
         printf("Cannot create shared memory!\n");
         exit(1);
     } else {
-        printf("Shared Memory Id = %d\n", shmid);
+        printf("Shared Memory Id = %d\n", shmid1);
+        printf("Shared Memory Id = %d\n", shmid2);
+        printf("Shared Memory Id = %d\n", shmid3);
     }
 
-    *count = 0; // init count
+    count = (int *) shmat(shmid1, NULL, 0);
+    *count = 0;
 
-    sem1 = (sem_t *)malloc(sizeof(sem_t));
+    sem1 = (sem_t *) shmat(shmid2, NULL, 0);
+    barrier = (sem_t *) shmat(shmid3, NULL, 0);
     sem_init(sem1, 1, 1);
-
-    barrier = (sem_t *)malloc(sizeof(sem_t));
     sem_init(barrier, 1, 0);
 }
 
@@ -39,6 +41,7 @@ void reach_barrier() {
     sem_post(sem1); // unlock the counter mutex –> other variables are free to access counter
     
     if (*count == nproc) {
+        *count = 0; // reset count to 0
         sem_post(barrier); // last process at the barrier sends a signal
     } else {
         sem_wait(barrier); // not the last process -> block cur process and wait until last process reaches
@@ -47,21 +50,23 @@ void reach_barrier() {
 }
 
 void destroy_barrier(int my_pid) {
-    if(my_pid != 0) {
-        // Destroy the semaphores and detach
-        // and free any shared memory. Notice
-        // that we explicity check that it is
-        // the parent doing it.
-
+    if (my_pid != 0) {
         // if parent, destroy semaphores
         sem_destroy(sem1);
-        free(sem1);
-
         sem_destroy(barrier);
-        free(barrier);
 
         // detach from SHM
         if (shmdt(count) == -1) {
+            perror("shmdt");
+            exit(EXIT_FAILURE);
+        }
+
+        if (shmdt(sem1) == -1) {
+            perror("shmdt");
+            exit(EXIT_FAILURE);
+        }
+
+        if (shmdt(barrier) == -1) {
             perror("shmdt");
             exit(EXIT_FAILURE);
         }
