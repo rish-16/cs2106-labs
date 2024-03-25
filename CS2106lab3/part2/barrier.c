2131b2fe3c07f729cc320f5c3eddf7711af1761e
Rishabh Anand <mail.rishabh.anand@gmail.com>
2024-03-26 02:27:54 +0800

    Updates

--- CS2106lab3/part2/barrier.c
+++ CS2106lab3/part2/barrier.c
@@ -5,69 +5,47 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 
-// sem_t *sem1; // the mutex semaphore
-// sem_t *barrier;
-// int *count;
-// int nproc;
-
-typedef struct {
-    sem_t *sem1; // the mutex semaphore
-    sem_t *barrier;
-    int *count;
-    int nproc;
-} BarrierObject;
-
-BarrierObject *barrier_object;
+sem_t *sem1; // the mutex semaphore
+sem_t *barrier;
+int *count;
+int nproc;
 
 void init_barrier(int numproc) {
-    // nproc = numproc;
-    int shmid;
-    shmid = shmget(IPC_PRIVATE, sizeof(BarrierObject), IPC_CREAT | 0666);
-    if (shmid == -1) {
+    nproc = numproc;
+
+    int shmid1, shmid2, shmid3;
+    shmid1 = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    shmid2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
+    shmid3 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
+    if (shmid1 == -1 || shmid2 == -1 || shmid3 == -1) {
         printf("Cannot create shared memory!\n");
         exit(1);
     } else {
-        printf("Shared Memory Id = %d\n", shmid);
+        printf("Shared Memory Id = %d\n", shmid1);
+        printf("Shared Memory Id = %d\n", shmid2);
+        printf("Shared Memory Id = %d\n", shmid3);
     }
 
-    barrier_object = (BarrierObject *)shmat(shmid, NULL, 0);
-    
-    barrier_object->nproc = numproc;
-    
-    barrier_object->count = (int *)malloc(sizeof(int));
-    *(barrier_object->count) = 0;
+    count = (int *) shmat(shmid1, NULL, 0);
+    *count = 0;
 
-    barrier_object->sem1 = (sem_t *)malloc(sizeof(sem_t));
-    barrier_object->barrier = (sem_t *)malloc(sizeof(sem_t));
-    sem_init(barrier_object->sem1, 1, 1);
-    sem_init(barrier_object->barrier, 1, 0);
+    sem1 = (sem_t *) shmat(shmid2, NULL, 0);
+    barrier = (sem_t *) shmat(shmid3, NULL, 0);
+    sem_init(sem1, 1, 1);
+    sem_init(barrier, 1, 0);
 }
 
-// void reach_barrier() {
-//     sem_wait(sem1);
-//     // *count = *count + 1; // increment count when new process has reached barrier
-//     (*count)++;
-//     sem_post(sem1); // unlock the counter mutex –> other variables are free to access counter
-    
-//     if (*count == nproc) {
-//         *count = 0; // reset count to 0
-//         sem_post(barrier); // last process at the barrier sends a signal
-//     } else {
-//         sem_wait(barrier); // not the last process -> block cur process and wait until last process reaches
-//         sem_post(barrier); // now that cur process is free, all other processes are released
-//     }
-// }
-
 void reach_barrier() {
-    sem_wait(barrier_object->sem1);
-    *(barrier_object->count) = *(barrier_object->count) + 1; // increment count when new processs reaches barrier
-    sem_post(barrier_object->sem1); // unlock the counter mutex –> other variables are free to access counter
+    sem_wait(sem1);
+    *count = *count + 1; // increment count when new process has reached barrier
+    sem_post(sem1); // unlock the counter mutex –> other variables are free to access counter
     
-    if (*(barrier_object->count) == barrier_object->nproc) {
-        sem_post(barrier_object->barrier); // last process at the barrier sends a signal
+    if (*count == nproc) {
+        *count = 0; // reset count to 0
+        sem_post(barrier); // last process at the barrier sends a signal
     } else {
-        sem_wait(barrier_object->barrier); // not the last process -> block cur process and wait until last process reaches
-        sem_post(barrier_object->barrier); // now that cur process is free, all other processes are released
+        sem_wait(barrier); // not the last process -> block cur process and wait until last process reaches
+        sem_post(barrier); // now that cur process is free, all other processes are released
     }
 }
 
@@ -79,19 +57,24 @@ void destroy_barrier(int my_pid) {
         // the parent doing it.
 
         // if parent, destroy semaphores
-        // sem_destroy(barrier_object->sem1);
-        // free(barrier_object->sem1);
+        sem_destroy(sem1);
+        free(sem1);
 
-        // sem_destroy(barrier_object->barrier);
-        // free(barrier_object->barrier);
+        sem_destroy(barrier);
+        free(barrier);
 
-        // // detach from SHM
-        // if (shmdt(barrier_object->count) == -1) {
-        //     perror("shmdt");
-        //     exit(EXIT_FAILURE);
-        // }
+        // detach from SHM
+        if (shmdt(count) == -1) {
+            perror("shmdt");
+            exit(EXIT_FAILURE);
+        }
 
-        if (shmdt(barrier_object) == -1){
+        if (shmdt(sem1) == -1) {
+            perror("shmdt");
+            exit(EXIT_FAILURE);
+        }
+
+        if (shmdt(barrier) == -1) {
             perror("shmdt");
             exit(EXIT_FAILURE);
         }
