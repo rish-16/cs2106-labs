d96f1a663cd23aeb7df8ce5a4b578bf238c31b27
Rishabh Anand <mail.rishabh.anand@gmail.com>
2024-03-26 02:29:20 +0800

    Updates

--- CS2106lab3/part2/barrier.c
+++ CS2106lab3/part2/barrier.c
@@ -51,11 +51,6 @@ void reach_barrier() {
 
 void destroy_barrier(int my_pid) {
     if (my_pid != 0) {
-        // Destroy the semaphores and detach
-        // and free any shared memory. Notice
-        // that we explicity check that it is
-        // the parent doing it.
-
         // if parent, destroy semaphores
         sem_destroy(sem1);
         sem_destroy(barrier);
